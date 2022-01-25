34d9f557fa93a0ab5353e29aa6cee978fbf1f18b
Eric Torre <etorreg@gmail.com>
2022-01-25 21:38:01 +0100

    fix tests

--- src/web/src/App.test.tsx
+++ src/web/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders books link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Books/i);
   expect(linkElement).toBeInTheDocument();
 });
